7e8b4d3eb78cc66ef8f2c851f6559290beaa10b4
Anthony <zaknofein2@gmail.com>
2017-06-05 21:04:05 +0300

    Fix image controller api

--- slace-app/src/main/java/ua/yware/slace/web/rest/ImageController.java
+++ slace-app/src/main/java/ua/yware/slace/web/rest/ImageController.java
@@ -16,24 +16,24 @@
 
 package ua.yware.slace.web.rest;
 
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import ua.yware.slace.service.storage.StorageService;
 
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-@Controller
-@RequestMapping("/image")
-@AllArgsConstructor
+@RestController
+@RequestMapping("/images")
+@RequiredArgsConstructor
 public class ImageController {
 
     private final StorageService storageService;
