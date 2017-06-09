c7cdbb691e1df01d9ecbf4fbfda55da1d83c9817
Anty Bond <zaknofein2@gmail.com>
2017-06-09 17:41:09 +0300

    Reconfigure project to build to a war

--- slace-app/src/main/java/ua/yware/slace/SlaceApplication.java
+++ slace-app/src/main/java/ua/yware/slace/SlaceApplication.java
@@ -5,10 +5,11 @@ import ua.yware.slace.service.storage.StorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @EnableConfigurationProperties(StorageProperties.class)
-public class SlaceApplication {
+public class SlaceApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SlaceApplication.class, args);
