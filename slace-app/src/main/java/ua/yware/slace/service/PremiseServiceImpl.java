413ef461fb310e0a767d9f5a3333c8ed37020370
Anty Bond <zaknofein2@gmail.com>
2017-06-09 15:22:32 +0300

    Change entity id types to big integer

--- slace-app/src/main/java/ua/yware/slace/service/PremiseServiceImpl.java
+++ slace-app/src/main/java/ua/yware/slace/service/PremiseServiceImpl.java
@@ -5,19 +5,16 @@ import org.springframework.stereotype.Service;
 import lombok.RequiredArgsConstructor;
 import ua.yware.slace.dao.PremiseRepository;
 import ua.yware.slace.model.Premise;
-import ua.yware.slace.search.PremiseSearchRepository;
 
 @Service
 @RequiredArgsConstructor
 public class PremiseServiceImpl implements PremiseService {
 
     private final PremiseRepository premiseRepository;
-    private final PremiseSearchRepository premiseSearchRepository;
 
     @Override
     public void save(Premise premise) {
         premiseRepository.save(premise);
-        premiseSearchRepository.index(premise);
     }
 
 }
