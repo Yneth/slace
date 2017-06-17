c8f05dffddac6fb944e69d8acbaf90355eb5b5e0
Anthony <zaknofein2@gmail.com>
2017-06-17 11:53:37 +0300

    Remove default error mvc autoconfiguration

--- slace-app/src/main/java/ua/yware/slace/SlaceApplication.java
+++ slace-app/src/main/java/ua/yware/slace/SlaceApplication.java
@@ -1,14 +1,9 @@
 package ua.yware.slace;
 
-import ua.yware.slace.service.storage.StorageProperties;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-@EnableConfigurationProperties(StorageProperties.class)
 public class SlaceApplication {
 
     public static void main(String[] args) {
